8bce5fa3ed5d1bfd6c906d19eb55ed0f23d8ba1f
Volkan YILMAZ <volkan.yilmazboun@gmail.com>
2018-03-04 21:28:40 +0300

    Update TinyURL.java

--- Res/TinyURL.java
+++ Res/TinyURL.java
@@ -1,1 +1,26 @@
-
+public class TinyURL {
+    Map<String, String> index = new HashMap<String, String>();
+    Map<String, String> revIndex = new HashMap<String, String>();
+    static String BASE_HOST = "http://tinyurl.com/"; 
+    public String encode(String longUrl) {
+        if (revIndex.containsKey(longUrl)) return BASE_HOST + revIndex.get(longUrl);
+        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        String key = null;
+        do {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 6; i++) { // 6 is the limit for creating new short adress
+                int r = (int) (Math.random() * alphabet.length());
+                sb.append(alphabet.charAt(r));
+            }
+            key = sb.toString();
+        }
+	 while (index. containsKey(key));
+        index.put(key, longUrl);
+        revIndex.put(longUrl, key);
+        return BASE_HOST + key;
+    }
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return index.get(shortUrl.replace(BASE_HOST, ""));
+    }
+}
